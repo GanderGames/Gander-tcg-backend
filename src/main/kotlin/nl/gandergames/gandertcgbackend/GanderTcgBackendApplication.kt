c629149152ccb8b89eb1d8a39f2a9e58d868d58d
The0Danktor <94552546+The0Danktor@users.noreply.github.com>
2024-03-04 00:48:38 +0100

    Initial setup

---
+++ src/main/kotlin/nl/gandergames/gandertcgbackend/GanderTcgBackendApplication.kt
@@ -0,0 +1,11 @@
+package nl.gandergames.gandertcgbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GanderTcgBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<GanderTcgBackendApplication>(*args)
+}
---
+++ src/test/kotlin/nl/gandergames/gandertcgbackend/GanderTcgBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package nl.gandergames.gandertcgbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GanderTcgBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
